0a0e06879b397a13396fb5eaadd9042e7e47f18c
AsTro0090 <157724448+AsTro0090@users.noreply.github.com>
2025-05-28 11:45:48 -0700

    add

---
+++ Assets/Script/Player/Girl/NewEmptyCSharpScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator animator;
+    public Rigidbody2D rb;
+
+    void Update()
+    {
+        float speed = Mathf.Abs(rb.linearVelocity.x);
+        animator.SetFloat("Speed", speed);
+
+        // التحقق من القفز
+        animator.SetBool("IsJumping", rb.linearVelocity.y > 0.1f);
+        animator.SetBool("IsFalling", rb.linearVelocity.y < -0.1f);
+    }
+}
