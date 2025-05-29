be2c31fd2fe8d36b3e90461036af29d9e2865f3f
AsTro0090 <157724448+AsTro0090@users.noreply.github.com>
2025-05-28 19:29:32 -0700

    صصصص

---
+++ Assets/Script/Player/Boy/New Folder/AttackEventBridge.cs
@@ -0,0 +1,22 @@
+﻿//using UnityEngine;
+
+//public class AttackEventBridge : MonoBehaviour
+//{
+//    public PlayerAnim playerAnim; // هذا هو الأب الذي يحمل الدالة
+
+//    public void DoAttack()
+//    {
+//        if (playerAnim != null)
+//        {
+//            playerAnim.DoAttack(); // ننفذ الدالة في الأب
+//        }
+//    }
+
+//    public void EndAttack()
+//    {
+//        if (playerAnim != null)
+//        {
+//            playerAnim.EndAttack();
+//        }
+//    }
+//}
---
+++ Assets/Script/Player/Boy/NewEmptyCSharpScript1.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class SimpleAttack : MonoBehaviour
+{
+    public Animator animator; // من الابن
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    public float attackDuration = 0.5f; // مدة أنميشن الهجوم (اضبطها يدويًا)
+
+    private bool isAttacking = false;
+
+    void Update()
+    {
+        if (isAttacking) return;
+
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            StartCoroutine(Attack());
+        }
+    }
+
+    System.Collections.IEnumerator Attack()
+    {
+        isAttacking = true;
+        animator.SetTrigger("Attack");
+
+        // انتظر مدة الأنميشن ثم ارجع للحركة
+        yield return new WaitForSeconds(attackDuration);
+
+        isAttacking = false;
+    }
+}
