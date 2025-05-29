afd24940c13295dd7a9de22c47f073361d40481e
AsTro0090 <157724448+AsTro0090@users.noreply.github.com>
2025-05-28 19:19:40 -0700

    ييي

--- Assets/Script/Player/Boy/BoyAttack.cs
+++ Assets/Script/Player/Boy/BoyAttack.cs
@@ -1,51 +1,28 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
 
-public class PlayerAttack : MonoBehaviour
-{
-    public Rigidbody2D rb;
-    public float moveSpeed = 5f;
-    public bool isAttacking = false;
+//public class AttackEventBridge : MonoBehaviour
+//{
+//    public Animator animator;
+//    public PlayerAttack playerAttack;
 
-    public Transform attackPoint;
-    public float attackRange = 0.5f;
-    public LayerMask enemyLayer;
+//    void Update()
+//    {
+//        // نشغل الأنميشن فقط إن طلب الأب ذلك
+//        if (playerAttack.isAttacking)
+//        {
+//            animator.SetTrigger("Attack");
+//        }
+//    }
 
-    void Update()
-    {
-        if (isAttacking) return;
+//    // تستدعى من Animation Event
+//    public void DoAttack()
+//    {
+//        playerAttack.DoAttack();
+//    }
 
-        float moveInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
-
-        // زر الهجوم
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            isAttacking = true;
-            // هنا لا نشغل الأنميشن، بل نتركه للابن عبر سكربت خاص
-        }
-    }
-
-    // تُستدعى من سكربت الابن عند لحظة الضربة
-    public void DoAttack()
-    {
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
-
-        foreach (Collider2D enemy in hitEnemies)
-        {
-            Debug.Log("ضرب العدو: " + enemy.name);
-            // enemy.GetComponent<Enemy>()?.TakeDamage(1);
-        }
-    }
-
-    public void EndAttack()
-    {
-        isAttacking = false;
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        if (attackPoint == null) return;
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
-    }
-}
+//    // نهاية الأنميشن
+//    public void EndAttack()
+//    {
+//        playerAttack.EndAttack();
+//    }
+//}
--- Assets/Script/Player/Boy/bridg.cs
+++ Assets/Script/Player/Boy/bridg.cs
@@ -1,14 +1,14 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
 
-public class AttackEventBridge : MonoBehaviour
-{
-    public PlayerAnim playerAnim; // ربط السكربت الأصلي
+//public class AttackEventBridge : MonoBehaviour
+//{
+//    public PlayerAnim playerAnim; // ربط السكربت الأصلي
 
-    public void EndAttack()
-    {
-        if (playerAnim != null)
-        {
-            playerAnim.EndAttack(); // استدعاء الدالة من الأب
-        }
-    }
-}
+//    public void EndAttack()
+//    {
+//        if (playerAnim != null)
+//        {
+//            playerAnim.EndAttack(); // استدعاء الدالة من الأب
+//        }
+//    }
+//}
