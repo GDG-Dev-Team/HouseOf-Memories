f9e80785f6364923514996c398c1fc60a2c38e19
AsTro0090 <157724448+AsTro0090@users.noreply.github.com>
2025-05-28 20:02:12 -0700

    شيشسيش

--- Assets/Script/Player/Boy/00000000.cs
+++ Assets/Script/Player/Boy/00000000.cs
@@ -1,6 +1,38 @@
-using UnityEngine;
+﻿//using UnityEngine;
 
-public class 00000000
-{
-    
-}
+//public class PlayerAttack : MonoBehaviour
+//{
+//    public Transform attackPoint;         // نقطة الضربة
+//    public float attackRange = 0.5f;      // نصف قطر الضربة
+//    public LayerMask enemyLayer;          // طبقة الأعداء
+
+//    void Update()
+//    {
+//        if (Input.GetKeyDown(KeyCode.X))  // زر الهجوم
+//        {
+//            Attack();
+//        }
+//    }
+
+//    void Attack()
+//    {
+//        // العثور على كل الأعداء ضمن منطقة الضربة
+//        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+
+//        foreach (Collider2D enemyCollider in hitEnemies)
+//        {
+//            Enemy enemy = enemyCollider.GetComponent<Enemy>();
+//            if (enemy != null)
+//            {
+//                enemy.TakeDamage(1); // أنقص من صحتهم 1
+//            }
+//        }
+//    }
+
+//    void OnDrawGizmosSelected()
+//    {
+//        if (attackPoint == null) return;
+//        Gizmos.color = Color.red;
+//        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+//    }
+//}
