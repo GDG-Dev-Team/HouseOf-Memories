a1256becd2b64f7a4a9a1317787be28612987db1
AsTro0090 <157724448+AsTro0090@users.noreply.github.com>
2025-05-28 20:44:29 -0700

    ارلارار

---
+++ Assets/Script/Player/Girl/SHOOT.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class AnimationEventRelay : MonoBehaviour
+{
+    public void FireBullet()
+    {
+        // الوصول إلى سكربت الأب الذي يحتوي على منطق الإطلاق
+        PlayerGunAttack gunAttack = GetComponentInParent<PlayerGunAttack>();
+        if (gunAttack != null)
+        {
+            gunAttack.FireBullet();
+        }
+        else
+        {
+            Debug.LogWarning("PlayerGunAttack not found in parent!");
+        }
+    }
+}
