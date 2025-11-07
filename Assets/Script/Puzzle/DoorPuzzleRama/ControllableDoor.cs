502653712f369d5df455f04284087c9f0275d5cc
Darl1ng-r <rama.tubeh.04@gmail.com>
2025-11-07 17:51:37 +0300

    .

--- Assets/Script/Puzzle/DoorPuzzleRama/ControllableDoor.cs
+++ Assets/Script/Puzzle/DoorPuzzleRama/ControllableDoor.cs
@@ -6,22 +6,16 @@ public class ControllableDoor : MonoBehaviour
     [Tooltip("The Particle System or VFX to play when the door is open.")]
     [SerializeField] private ParticleSystem openVFX;
 
-    // We will get these components automatically.
-    private SpriteRenderer spriteRenderer;
+
     private Collider2D doorCollider;
 
     // Awake is called before the first frame update.
     private void Awake()
     {
-        // Get references to this object's own components.
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        doorCollider = GetComponent<Collider2D>();
+       
+        doorCollider = GetComponent<BoxCollider2D>();
 
-        // Error checking to make sure the door is set up correctly.
-        if (spriteRenderer == null)
-        {
-            Debug.LogError("ControllableDoor is missing a SpriteRenderer component!");
-        }
+        
         if (doorCollider == null)
         {
             Debug.LogError("ControllableDoor is missing a Collider2D component!");
@@ -31,8 +25,7 @@ public class ControllableDoor : MonoBehaviour
     // A public function that can be called by other scripts to open the door.
     public void Open()
     {
-        // Deactivate the sprite and collider.
-        spriteRenderer.enabled = false;
+        
         doorCollider.enabled = false;
 
         // If a VFX has been assigned, play it.
@@ -45,8 +38,7 @@ public class ControllableDoor : MonoBehaviour
     // A public function that can be called by other scripts to close the door.
     public void Close()
     {
-        // Re-activate the sprite and collider.
-        spriteRenderer.enabled = true;
+        
         doorCollider.enabled = true;
 
         // If a VFX has been assigned, stop it.
