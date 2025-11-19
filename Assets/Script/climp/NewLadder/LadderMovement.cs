e4d81161bcb97c03e744a3af0721aa7f49d0f0d8
dana jihad alhaddar <danahaddar220@gmail.com>
2025-11-19 19:55:23 +0300

    Ladders

---
+++ Assets/Script/climp/NewLadder/LadderMovement.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class LadderMovement : MonoBehaviour
+{
+
+    private float vertical;
+    private bool isLadder;
+    private bool isClimbing;
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private Rigidbody2D rb;
+   
+
+    void Update()
+    {
+        vertical = Input.GetAxis("Vertical");
+        if(isLadder && Mathf.Abs(vertical) > 0f)
+        {
+            isClimbing = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isClimbing)
+        {
+            rb.gravityScale = 0f;
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, vertical * speed);
+
+        }
+        else
+        {
+            rb.gravityScale = 4f;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            isLadder = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            isLadder = false;
+            isClimbing = false;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
